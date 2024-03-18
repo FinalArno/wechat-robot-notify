effd7f53aa0bc0a77bd71023275bdfc7fe95c77a
arno <lupingtu@yunify.com>
2024-03-18 13:51:06 +0800

    feat: init

---
+++ app/controller/home.js
@@ -0,0 +1,48 @@
+const { Controller } = require("egg");
+
+class HomeController extends Controller {
+  async hook(body, query) {
+    // 组织消息要发送的信息，支持 markdown
+    const data = {
+      msgtype: "markdown",
+      markdown: {
+        content: `##小飞棍来了`,
+      },
+    };
+    const { key } = query;
+    if (!key) {
+      return;
+    }
+    // 通知企业微信机器人
+    await this.ctx.curl(
+      `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${key}` /* 机器人的 webhook url */,
+      {
+        method: "POST",
+        contentType: "json",
+        data,
+        dataType: "json",
+      }
+    );
+    return true;
+  }
+
+  async index() {
+    const { ctx } = this;
+    ctx.body = "Hello Index";
+  }
+
+  async webhook() {
+    const { ctx } = this;
+    const body = ctx.request.body;
+    const query = ctx.request.query;
+    console.log("body---", body, query);
+    if (body.event_type === "push_event") {
+      await this.hook(body, query);
+      ctx.body = "Hello Webhook";
+    } else {
+      ctx.body = "Hello Webhook";
+    }
+  }
+}
+
+module.exports = HomeController;
---
+++ config/config.default.js
@@ -0,0 +1,43 @@
+/* eslint valid-jsdoc: "off" */
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = (appInfo) => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {});
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + "_1710731276182_977";
+
+  // add your middleware config here
+  config.middleware = [];
+
+  config.cluster = {
+    listen: {
+      port: 1128,
+    },
+  };
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+    xframe: {
+      enable: false,
+    },
+  };
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
